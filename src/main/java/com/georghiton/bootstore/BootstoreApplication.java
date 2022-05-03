61b15df6563062463866039cd4e9d4eda674354e
georghiton-luiz <georghiton@hotmail.com>
2022-05-02 23:07:59 -0300

    Commit inicial

---
+++ src/main/java/com/georghiton/bootstore/BootstoreApplication.java
@@ -0,0 +1,13 @@
+package com.georghiton.bootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/georghiton/bootstore/BootstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georghiton.bootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
